98fbd48a290363b968b6e56d64e99eecc941b05f
Andrei <andrei.nekrasch@yandex.ru>
2024-09-02 13:42:19 +0300

    errorMessage

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
-function f1()
-{
-    alert("f1");
+function isFillen(event) {
+    let name = document.querySelector("#name").value;
+    let email = document.querySelector("#email").value;
+    let password = document.querySelector("#password").value;
+
+    // Проверяем, введено ли "John" в поле name
+    if (name === "John") {
+        event.preventDefault(); // Предотвращаем отправку формы
+        document.querySelector("#message").style.color="red";
+       document.querySelector("#message").innerHTML="Name field cannot contain 'John'"
+    }
 }
