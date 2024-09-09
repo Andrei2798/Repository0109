f41555c85907eea857f9d01ae11843f62727a301
Andrei <andrei.nekrasch@yandex.ru>
2024-09-09 23:36:20 +0300

    hello

---
+++ public/js/main.js
@@ -0,0 +1,40 @@
+function isFillen(event) {
+    let name = document.querySelector("#name").value;
+    let email = document.querySelector("#email").value;
+    let password = document.querySelector("#password").value;
+
+    // Проверяем, введено ли "John" в поле name
+    if (name === "John"||name=="" ) {
+        event.preventDefault(); // Предотвращаем отправку формы
+        document.querySelector("#message").style.color="red";
+       document.querySelector("#message").innerHTML="Name field cannot contain 'John' or be empty"
+    }
+}
+
+
+// Число, которое нужно отправить на сервер
+const numberToSend = 83; // Можно указать любое число
+
+async function sendNumber() {
+    try {
+        // Отправляем POST-запрос на сервер
+        const response = await fetch('http://localhost:3000/number', {
+            method: 'POST', // Метод запроса
+            headers: {
+                'Content-Type': 'application/json' // Устанавливаем тип контента JSON
+            },
+            body: JSON.stringify({ number: numberToSend }) // Преобразуем число в JSON и отправляем
+        });
+
+        // Ожидаем ответ от сервера
+        const data = await response.json();
+
+        // Выводим ответ от сервера в консоль
+        console.log(`Server responded with: ${data.number}`);
+    } catch (error) {
+        console.error('Error:', error); // В случае ошибки выводим ее в консоль
+    }
+}
+
+// Вызываем функцию отправки числа сразу после загрузки скрипта
+sendNumber();
--- server.js
+++ server.js
@@ -1,1 +1,22 @@
-console.log("Hello");
+const express = require ("express");
+const app = express();
+const port = 3000;
+
+app.use((req, res, next) => {
+    console.log(`\n--- ${req.url} Time: ${Date.now()} `);
+    next(); //передача управления следующей функции-middleware
+  });
+  
+  app.use(express.static(__dirname + "/public"));
+  
+  app.use(express.json()); //middleware для работы с body из fetch клиента
+
+
+app.post('/number', (req, res) => {
+  const number = req.body.number; // Получаем число из запроса
+  res.json({ number: number-1 }); // Отправляем это же число обратно
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
