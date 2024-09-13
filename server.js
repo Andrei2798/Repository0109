bc99097960cd571d57d22eaf986cb42dc57acbdc
Andrei <andrei.nekrasch@yandex.ru>
2024-09-13 15:33:36 +0300

    ||

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require ("express");
 const cors = require("cors");
 const app = express();
-const port = 3000;
+// const port = 3000;
+const port = process.env.PORT || 3000; // Используй PORT из окружения
 
 app.use(cors()); // Включаем CORS для всех доменов
 
